5ed962396d9821a8ff769981006350cb489ae739
Viktory <mokshan.os@mail.ru>
2025-08-05 15:17:43 +0300

    CRUD

--- training/views.py
+++ training/views.py
@@ -1,3 +1,36 @@
+import generics
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from training.models import Course, Lesson
+from training.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonCreateAPIView(generics, CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics, ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(generics, RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(generics, UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(generics, DestroyAPIView):
+    queryset = Lesson.objects.all()
