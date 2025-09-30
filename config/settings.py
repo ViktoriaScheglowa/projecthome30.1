91f8d70c9702db0ba6e7facbc873c9d6dcc24f70
Viktory <mokshan.os@mail.ru>
2025-09-30 19:00:09 +0300

    выполнение задания 35,2

--- config/settings.py
+++ config/settings.py
@@ -198,7 +198,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "STATIC_ROOT": os.path.join(BASE_DIR, "static"),
     }
 }
