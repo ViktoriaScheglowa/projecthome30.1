64ae275b34e147dd7c9f54b64b341c963996bcbb
Viktory <mokshan.os@mail.ru>
2025-10-01 14:36:40 +0300

    выполнение задания 35,2

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "NAME": os.getenv("NAME"),
         "USER": os.getenv("USER"),
         "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": "db",
+        "HOST": os.getenv("HOST", "db"),
         "PORT": os.getenv("PORT"),
     }
 }
