5a66cbeb4f5934b5e21b503d5050948bb87cb261
Viktory <mokshan.os@mail.ru>
2025-10-02 20:10:58 +0300

    выполнение задания 35,2

--- config/settings.py
+++ config/settings.py
@@ -18,37 +18,37 @@ ALLOWED_HOSTS = ["*"]
 
 def get_database_config():
     """Гибкая конфигурация БД для разных окружений"""
-    if os.getenv('GITHUB_ACTIONS') or os.getenv('CI'):
+    if os.getenv("GITHUB_ACTIONS") or os.getenv("CI"):
         print("GITHUB_ACTIONS")
         return {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('NAME', 'test_db'),
-            'USER': os.getenv('USER', 'postgres'),
-            'PASSWORD': os.getenv('PASSWORD', 'postgres'),
-            'HOST': os.getenv('HOST', 'localhost'),
-            'PORT': os.getenv('PORT', '5432'),
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("NAME", "test_db"),
+            "USER": os.getenv("USER", "postgres"),
+            "PASSWORD": os.getenv("PASSWORD", "postgres"),
+            "HOST": os.getenv("HOST", "localhost"),
+            "PORT": os.getenv("PORT", "5432"),
         }
     # Если запуск в Docker
-    elif os.getenv('DOCKER_ENV'):
+    elif os.getenv("DOCKER_ENV"):
         print("DOCKER_ENV")
         return {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('NAME'),
-            'USER': os.getenv('USER'),
-            'PASSWORD': os.getenv('PASSWORD'),
-            'HOST': 'db',
-            'PORT': os.getenv('PORT', '5432'),
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("NAME"),
+            "USER": os.getenv("USER"),
+            "PASSWORD": os.getenv("PASSWORD"),
+            "HOST": "db",
+            "PORT": os.getenv("PORT", "5432"),
         }
     # Локальная разработка (не в Docker)
     else:
         print("не в Docker")
         return {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('NAME', 'local_db'),
-            'USER': os.getenv('USER', 'postgres'),
-            'PASSWORD': os.getenv('PASSWORD', 'postgres'),
-            'HOST': os.getenv('HOST', 'localhost'),
-            'PORT': os.getenv('PORT', '5432'),
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("NAME", "local_db"),
+            "USER": os.getenv("USER", "postgres"),
+            "PASSWORD": os.getenv("PASSWORD", "postgres"),
+            "HOST": os.getenv("HOST", "localhost"),
+            "PORT": os.getenv("PORT", "5432"),
         }
 
 
@@ -119,9 +119,7 @@ REST_FRAMEWORK = {
 }
 
 
-DATABASES = {
-    'default': get_database_config()
-}
+DATABASES = {"default": get_database_config()}
 
 print("-----------DATABASES-----------")
 print(get_database_config())
