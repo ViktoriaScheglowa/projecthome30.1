fc3e1f7ab9dbf7844ec4067d1643a12a6289b4d9
Viktory <mokshan.os@mail.ru>
2025-10-03 18:45:49 +0300

    выполнение задания 35,2

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ def get_database_config():
             "NAME": os.getenv("NAME", "local_db"),
             "USER": os.getenv("USER", "postgres"),
             "PASSWORD": os.getenv("PASSWORD", "postgres"),
-            "HOST": os.getenv("HOST", "localhost"),
+            "HOST": os.getenv("HOST", "db"),
             "PORT": os.getenv("PORT", "5432"),
         }
 
